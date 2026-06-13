deacc09321f25c8bc565a5f8afbf9c5addb18def
Vikash Singh <vikash@gmail.com>
2026-06-13 23:37:02 +0530

    llm

---
+++ Langchain/models/LLMs/llm_groq.py
@@ -0,0 +1,28 @@
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+client = Groq()
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        # Set an optional system message. This sets the behavior of the
+        # assistant and can be used to provide specific instructions for
+        # how it should behave throughout the conversation.
+        {
+            "role": "system",
+            "content": "You are a helpful assistant."
+        },
+        # Set a user message for the assistant to respond to.
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+
+    # The language model which will generate the completion.
+    model="llama-3.3-70b-versatile"
+)
+
+# Print the completion returned by the LLM.
+print(chat_completion.choices[0].message.content)
